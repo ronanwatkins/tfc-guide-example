5b11ca0b7a95f842a439f71b712c2003c8c1e56e
Ronan Watkins <ronan.watkins@genesys.com>
2021-12-09 11:40:09 +0000

    Export config

--- main.tf
+++ main.tf
@@ -1,28 +1,11 @@
-provider "aws" {
-  region = var.region
+
+provider "genesyscloud" {
+  sdk_debug = true
 }
 
-data "aws_ami" "ubuntu" {
-  most_recent = true
-
-  filter {
-    name   = "name"
-    values = ["ubuntu/images/hvm-ssd/ubuntu-focal-20.04-amd64-server-*"]
-  }
-
-  filter {
-    name   = "virtualization-type"
-    values = ["hvm"]
-  }
-
-  owners = ["099720109477"] # Canonical
-}
-
-resource "aws_instance" "ubuntu" {
-  ami           = data.aws_ami.ubuntu.id
-  instance_type = var.instance_type
-
-  tags = {
-    Name = var.instance_name
-  }
+resource "genesyscloud_tf_export" "export" {
+  include_state_file = true
+  resource_types = [
+    "genesyscloud_architect_user_prompt"
+  ]
 }
